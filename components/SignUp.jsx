4d28b423cc98fde0297863cbcaa5ef91be2dfd2f
bahstiien <bastien.lecalvez@gmail.com>
2021-12-09 19:37:16 +0100

    init css on the form

--- components/SignUp.jsx
+++ components/SignUp.jsx
@@ -27,62 +27,60 @@ export default function SignUp() {
   });
 
   const onSubmit = async (data) => {
-    axios
-      .post("http://localhost://3000/users")
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => {
-        // Handle Error Here
-        console.error(err);
-      });
+    console.log(data);
+    // axios
+    //   .post("/api/users", data)
+    //   .then((res) => {
+    //     console.log(res.data);
+    //     alert("all ok");
+    //   })
+    //   .catch((err) => {
+    //     console.error(err);
+    //   });
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <h1> Inscription</h1>
 
-      <div className="m-10 border-2 w-3/5">
+      <div className="m-10 w-2/4 p-4 flex flex-row items-center">
         <label htmlFor="name" className="m-2">
           Nom * :
         </label>
-
         <input
-          className="w-2/5 m-2"
+          className="border-2 rounded-md m-2 p-1 border-gray-100 w-full"
           type="text"
-          id="LastName"
-          name="LastName"
-          {...register("LastName", {
+          id="lastname"
+          name="lastname"
+          {...register("lastname", {
             required: " ❌ Champs obligatoire ",
           })}
         />
-        {errors.LastName && <span> {errors.LastName.message}</span>}
+        {errors.lastname && <span> {errors.lastname.message}</span>}
       </div>
 
-      <div className="m-10 border-2 w-3/5">
+      <div className="m-10 w-2/4 p-4 flex flex-row items-center">
         <label htmlFor="name" className="m-2">
           Prénom * :
         </label>
-
         <input
-          className="w-2/5 m-2"
+          className="border-2 rounded-md m-2 p-1 border-gray-100 w-full"
           type="text"
-          id="FirstName"
-          name="FirstName"
-          {...register("FirstName", {
+          id="firstname"
+          name="firstname"
+          {...register("firstname", {
             required: " ❌ Champs obligatoire ",
           })}
         />
-        {errors.FirstName && <span> {errors.FirstName.message}</span>}
+        {errors.firstname && <span> {errors.firstname.message}</span>}
       </div>
 
-      <div className="m-10 border-2 w-3/5">
+      <div className="m-10 w-2/4 p-4 flex flex-row items-center">
         <label htmlFor="email" className="m-2">
-          {" "}
           Email :
         </label>
         <input
-          className="w-2/5 m-2"
+          className="border-2 rounded-md m-2 p-1 border-gray-100 w-full"
           type="text"
           id="email"
           name="email"
@@ -96,123 +94,149 @@ export default function SignUp() {
         />
         {errors.email && <span> {errors.email.message}</span>}
       </div>
-      <div className="m-10 border-2 w-3/5">
-        <label htmlFor="password" className="m-2">
-          {" "}
-          Votre mot de passe
-        </label>
-        <input
-          className="w-2/5 m-2"
-          id="password"
-          type={passwordShown ? "text" : "password"}
-          {...register("password", {
-            required: " ❌ Password requis ",
-            pattern: {
-              value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/,
-              message: "❌ Le mot de passe n'est pas au bon format",
-            },
-          })}
-        />
-        <RemoveRedEyeIcon onClick={togglePasswordVisiblity} />
-        {errors.password && <span> {errors.password.message}</span>}
 
-        <label>Repeat password</label>
-        <input
-          name="confirmPassword"
-          type={passwordConfirmShown ? "text" : "password"}
-          {...register("password", {
-            required: " ❌ Password requis ",
-            pattern: {
-              value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/,
-              message: "❌ Le mot de passe n'est pas au bon format",
-            },
-          })}
-        />
-        <RemoveRedEyeIcon onClick={togglePasswordConfirmVisiblity} />
-        {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
-        <p> Le mot de passe doit contenir</p>
-        <p> 1 chiffre / 1 caractère spécial / 1 Majuscule / 8 caractères </p>
+      <div className="m-10 w-2/4 p-4 flex flex-col items-center">
+        <div className="flex items-center justify-around">
+          <label htmlFor="password" className="">
+            Votre mot de passe
+          </label>
+          <input
+            className="border-2 rounded-md m-2 p-1 border-gray-100 w-full"
+            id="password"
+            type={passwordShown ? "text" : "password"}
+            {...register("password", {
+              required: " ❌ Password requis ",
+              pattern: {
+                value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/,
+                message: "❌ Le mot de passe n'est pas au bon format",
+              },
+            })}
+          />
+          <RemoveRedEyeIcon onClick={togglePasswordVisiblity} />
+          {errors.password && <span> {errors.password.message}</span>}
+        </div>
+
+        <div className="flex items-center justify-around mb-4">
+          <label>Confirmez</label>
+          <input
+            className="border-2 rounded-md m-2 p-1 border-gray-100 w-full"
+            name="confirmPassword"
+            type={passwordConfirmShown ? "text" : "password"}
+            {...register("password", {
+              required: " ❌ Password requis ",
+              pattern: {
+                value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/,
+                message: "❌ Le mot de passe n'est pas au bon format",
+              },
+            })}
+          />
+          <RemoveRedEyeIcon onClick={togglePasswordConfirmVisiblity} />
+          {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
+        </div>
+
+        <div className="text-sm italic text-center p-4">
+          <p> Le mot de passe doit contenir</p>
+          <p> 1 chiffre / 1 caractère spécial / 1 Majuscule / 8 caractères </p>
+        </div>
       </div>
 
-      <div>
-        <select>
-          <option value="2">Particulier</option>
-          <option value="3">Collectivité</option>
-          <option value="3">Association</option>``
-          <option value="4"> Entreprise</option>
-        </select>
+      <div className="m-10 border-2 w-1/4 p-4 rounded-xl shadow-md ">
+        <div className="flex flex-col">
+          <div className="flex items-center justify-around mb-4">
+            <label htmlFor="organizationType"> Type :</label>
+            <select>
+              <option value="2">Particulier</option>
+              <option value="3">Collectivité</option>
+              <option value="3">Association</option>``
+              <option value="4"> Entreprise</option>
+            </select>
+          </div>
+          <div className="flex items-center justify-around mb-4">
+            <label htmlFor="managerName"> Nom du responsable</label>
+            <input
+              className="border-2 rounded-md m-2 p-1 border-gray-100"
+              type="text"
+              id="managerName"
+              name="managerName"
+            />
+          </div>
 
-        <label htmlFor="boss"> Nom du responsable</label>
-        <input type="text" id="boss" name="boss" />
-
-        <label htmlFor="nameOfCie"> Dénomination</label>
-        <input type="text" id="nameOfCie" name="nameOfCie" />
-
-        <label htmlFor="siret"> Siret : </label>
-        <input type="number" />
+          <div className="flex items-center justify-around mb-4">
+            <label htmlFor="organizationName"> Dénomination</label>
+            <input
+              className="border-2 rounded-md m-2 p-1 w-3/5 border-gray-100"
+              type="text"
+              id="organizationName"
+              name="organizationName"
+            />
+          </div>
+          <label htmlFor="siret"> Siret : </label>
+          <input
+            className="border-2 rounded-md m-2 w-3/5 p-1 border-gray-100"
+            type="number"
+            name="siretNumber"
+            id="siretNumber"
+          />
+        </div>
       </div>
 
-      <div>
-        <label htmlFor="phone"> Téléphone : </label>
+      <div className="m-10 border-2 w-3/5 p-4">
+        {" "}
+        <div>
+          <label htmlFor="adrresse"> adresse :</label>
+          <input
+            className="w-2/5 m-2"
+            type="text"
+            id="address1"
+            name="address1"
+          />
+          <label> Complément :</label>
+          <input
+            className="w-2/5 m-2"
+            type="text"
+            id="address2"
+            name="address2"
+          />
+          {errors.address1 && <span> {errors.address1.message}</span>}
+        </div>
+        <div>
+          <label htmlFor="zipCode"> code postal</label>
+          <input
+            type="number"
+            id="zipCode"
+            name="zipCode"
+            {...register("zipCode", {
+              required: " ❌   Entrez une code postal valide ",
+              maxLength: 5,
+            })}
+          />
+        </div>
+        <div>
+          <label htmlFor="city"> Ville : </label>
+          <input type="text" id="city" name="city" />
+        </div>
+        <div>
+          <label htmlFor="phone"> Téléphone : </label>
 
-        <Controller
-          control={control}
-          name="phone"
-          render={({
-            field: { onChange, value, name, ref },
-            fieldState: { invalid, isTouched, isDirty, error },
-            formState,
-          }) => (
-            <PhoneInput country={"fr"} selected={value} onChange={onChange} />
-          )}
-        />
+          <Controller
+            control={control}
+            name="phone"
+            id="phone"
+            render={({
+              field: { onChange, value, name, ref },
+              fieldState: { invalid, isTouched, isDirty, error },
+              formState,
+            }) => (
+              <PhoneInput country={"fr"} selected={value} onChange={onChange} />
+            )}
+          />
+        </div>
       </div>
 
-      <div>
-        <label htmlFor="adrresse"> adresse :</label>
-        <input
-          className="w-2/5 m-2"
-          type="text"
-          id="adrresse"
-          name="adrresse"
-        />
-        <label> Complément :</label>
-        <input
-          className="w-2/5 m-2"
-          type="text"
-          id="adrresse2"
-          name="adrresse2"
-        />
-        {errors.adrresse && <span> {errors.adrresse.message}</span>}
-      </div>
-
-      <div>
-        <label htmlFor="Cp"> code postal</label>
-        <input
-          type="number"
-          id="codePostal"
-          name="codePostal"
-          {...register("codePostal", {
-            required: " ❌   Entrez une code postal valide ",
-            maxLength: 5,
-          })}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="city"> Ville : </label>
-        <input
-          type="text"
-          id="city"
-          name="city"
-          // {...register("city", {
-          //   required: " ❌   Entrez une ville  valide ",
-          //   maxLength: 5,
-          // })}
-        />
-      </div>
-
-      <button type="submit"> Créer un compte</button>
+      <button type="submit" className=".ctaBtn">
+        {" "}
+        Créer un compte
+      </button>
     </form>
   );
 }
