fbaa7d8093ce6217e6e5f094ffcd8ade6ab18c9f
Selena <selena.poun@gmail.com>
2021-12-14 09:20:08 +0100

    update navbar logo

--- pages/navbar.js
+++ pages/navbar.js
@@ -1,20 +1,19 @@
 import Link from "next/link";
-import { AiOutlineLogin } from "react-icons";
-import { IoIosAddCircle } from "react-icons";
+import { AiOutlineLogin } from "react-icons/ai";
+import { IoIosAddCircle } from "react-icons/io";
 import Image from "next/image";
 import LogoSansBlabla from "../public/assets/LogoSansBlabla.png";
 
 export default function Navbar() {
   return (
-    <div className="w-full flex flex-row items-center p-2 justify-between bg-white shadow-xs bg-indigo-500">
+    <div className="w-full flex flex-row items-center p-2 justify-between bg-white shadow-xs bg-indigo-500 ">
       <Image src={LogoSansBlabla} alt="Logo" width="100px" height="100px" />
       <div className="ml-8 text-lg text-white hidden md:flex">
         {" "}
         <Link href="/Devis">
-          <a>
-            Devis <IoIosAddCircle />
-          </a>
+          <a>Devis</a>
         </Link>
+        <IoIosAddCircle />
       </div>
       <span className="w-full md:w-1/3 h-10 cursor-pointer border border-gray-300 text-sm rounded-full flex">
         <input
@@ -25,20 +24,7 @@ export default function Navbar() {
         />
       </span>
 
-      <div className="flex flex-row-reverse text-white mr-4 ml-4 md:hidden">
-        <button>
-          <svg
-            width="20"
-            height="20"
-            fill="currentColor"
-            className="h-8 w-8"
-            viewBox="0 0 1792 1792"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z"></path>
-          </svg>
-        </button>
-      </div>
+      <div className="flex flex-row-reverse text-white mr-4 ml-4 md:hidden"></div>
       <div className="flex items-center mr-8 hidden md:flex">
         <Link href="/Connexion"> Connexion</Link>
         <a href="#" className="">
