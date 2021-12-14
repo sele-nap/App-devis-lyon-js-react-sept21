0777a7ade1e3046234d37baf71bcdbde22c4165d
Selena <selena.poun@gmail.com>
2021-12-14 11:48:03 +0100

    update tailwind

--- pages/navbar.js
+++ pages/navbar.js
@@ -6,29 +6,31 @@ import LogoSansBlabla from "../public/assets/LogoSansBlabla.png";
 
 export default function Navbar() {
   return (
-    <div className="w-full flex flex-row items-center p-2 justify-between bg-white shadow-xs bg-indigo-500 ">
-      <Image src={LogoSansBlabla} alt="Logo" width="100px" height="100px" />
-      <div className="ml-8 text-lg text-white hidden md:flex">
+    <div className="w-full flex flex-row items-center p-0 justify-between shadow-xs bg-third">
+      <Image src={LogoSansBlabla} alt="Logo" width="50px" height="50px" />
+      <div className="text-black flex items-center m-2 md:flex">
         {" "}
         <Link href="/Devis">
           <a>Devis</a>
         </Link>
-        <IoIosAddCircle />
+        <a href="#" className="pl-2">
+          <IoIosAddCircle size={28} />
+        </a>
       </div>
-      <span className="w-full md:w-1/3 h-10 cursor-pointer border border-gray-300 text-sm rounded-full flex">
+      <span className="w-full md:w-1/ sm:w-1/6 h-10 cursor-pointer border border-gray-300 text-sm rounded-full flex">
         <input
           type="search"
           name="serch"
-          placeholder="Search"
+          placeholder="Recherche"
           className="flex-grow px-4 rounded-l-full rounded-r-full text-sm focus:outline-none"
         />
       </span>
 
-      <div className="flex flex-row-reverse text-white mr-4 ml-4 md:hidden"></div>
-      <div className="flex items-center mr-8 hidden md:flex">
+      <div className="flex flex-row-reverse text-black mr-4 ml-4 md:hidden"></div>
+      <div className="flex items-center m-2 md:flex">
         <Link href="/Connexion"> Connexion</Link>
-        <a href="#" className="">
-          <AiOutlineLogin />
+        <a href="#" className="pl-2">
+          <AiOutlineLogin size={28} />
         </a>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      colors: {
+        primary: "#F9F7F3",
+        secondary: "#ECE6E6",
+        third: "#DAB455",
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
