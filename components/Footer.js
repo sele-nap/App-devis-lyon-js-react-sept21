48b9896dfa0cc79c6a9c7b07916ba27042a9fdcb
arthaud14 <77742592+arthaud14@users.noreply.github.com>
2021-12-14 11:51:06 +0100

    Changed footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,38 +6,38 @@ export default function Footer() {
        
 
 
-<footer className="bg-third absolute bottom-0 left-0 dark:bg-gray-800 w-full py-8">
-    <div className="max-w-screen-xl mx-auto px-4">
-        <ul className="max-w-screen-md mx-auto text-lg font-light flex flex-wrap justify-between">
-            <li className="my-2">
+<footer className="bg-third fixed bottom-0 left-0 dark:bg-gray-800 w-full py-8">
+    <div className="max-w-screen-xl mx-auto px-4 h-0">
+        <ul className="flex flex-wrap justify-between">
+            <li className="">
             <Link href="./catalog">
                 <a className="text-black-400 hover:text-black-800 dark:text-gray-300 dark:hover:text-white transition-colors duration-200" href="#">
                     Catalogue
                 </a>
                 </Link>
             </li>
-            <li className="my-2">
+            <li className="">
             <Link href="./cgv">
                 <a className="text-black-400 hover:text-gray-800 dark:text-gray-300 dark:hover:text-white transition-colors duration-200" href="#">
                     CGV
                 </a>
                 </Link>
             </li>
-            <li className="my-2">
+            <li className="">
             <Link href="./about">
                 <a className="text-black-400 hover:text-gray-800 dark:text-gray-300 dark:hover:text-white transition-colors duration-200" href="#">
                     Qui sommes nous
                 </a>
                 </Link>
             </li>
-            <li className="my-2">
+            <li className="">
                 <Link href="./legal">
                 <a className="text-black-400 hover:text-gray-800 dark:text-gray-300 dark:hover:text-white transition-colors duration-200" href="#">
                     Mentions légales
                 </a>
                 </Link>
             </li>
-            <li className="my-2">
+            <li className="">
                 <Link href="./contact">
                 <a className="text-black-400 hover:text-gray-800 dark:text-gray-300 dark:hover:text-white transition-colors duration-200" href="#">
                     Nous contacter
