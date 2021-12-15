03615c157c42194bfebb23c50ede26b024d81773
solene Verrier <sosso711@hotmail.com>
2021-12-15 15:17:56 +0100

    responsive design

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import ImageEcharpe from "../public/Img/echarpe.jpeg";
 import ImageMedaille from "../public/Img/medaille.jpg";
 import Layout from "../components/Layout";
 import Link from "next/link";
+import ImageTrophée from "../public/Img/imagetrophée.jpg";
 
 export default function Home() {
   return (
@@ -16,12 +17,20 @@ export default function Home() {
           {" "}
           maison de tradition lyonnaise de qualité vous souhaite la bienvenue
         </h1>
-        <div className="h-40 w-full shadow-2xl shadow-black bg-gray-200">
+        <div className="h-40 w-full ">
           <div className="h-40">carroussel</div>
 
-          <div className="bg-primary flex flex-row-reverse justify-around p-5 border-solid border-purple-300">
-            <Image src={ImageDrapeau} width="100px" height="100px" />
-            <p className="w-1/3 text-justify">
+          <div className="bg-primary flex flex-row-reverse justify-around p-5 ">
+            <div className="hidden md:block rounded-xl">
+              <Image
+                className="rounded-xl"
+                src={ImageTrophée}
+                width="200px"
+                height="200px"
+              />
+            </div>
+
+            <p className=" w-full md:w-1/2 text-justify">
               "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
               accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
               quae ab illo inventore veritatis et quasi architecto beatae vitae
@@ -32,42 +41,52 @@ export default function Home() {
           </div>
           <div className="bg-secondary ">
             <div className=" ">
-              <h2 className="text-center text-xl m-3 pt-3">
-                Produit du moment
+              <h2 className="text-center text-xl m-3 pt-8">
+                Produits du moment
               </h2>
               <div className=" h-full p-7 w-full m-100">
-                <div className=" border-solid border-2 border-black rounded-xl p-5 md:border-none grid overflow-hidden grid-cols-2 grid-rows-2 gap-6 pb">
+                <div className=" border-solid border-2 border-black rounded-xl p-5  sm: grid overflow-hidden grid-cols-2 grid-rows-2 gap-6  md:border-none ">
                   <Link href="/catalog">
                     <a className="flex justify-center md:justify-end ">
                       <Image
                         src={ImageDrapeau}
                         width="200px"
                         height="200px "
-                        className="box col-start-1"
+                        className="box col-start-1 rounded-xl  border-4 border-black "
                       />
                     </a>
                   </Link>
 
                   <Link href="/catalog">
-                    <a className="flex justify-center md:justify-between">
-                      <Image src={ImageEcharpe} width="200px" height="200px" />
+                    <a className="flex justify-center md:justify-between ">
+                      <Image
+                        src={ImageEcharpe}
+                        width="200px"
+                        height="200px"
+                        className="rounded-xl"
+                      />
                     </a>
                   </Link>
 
                   <Link href="/catalog">
-                    <a className="flex justify-center md:justify-end">
+                    <a className="flex justify-center md:justify-end ">
                       <Image
                         src={ImageMedaille}
                         width="200px"
                         height="200px"
-                        className=""
+                        className="rounded-xl"
                       />
                     </a>
                   </Link>
 
                   <Link href="/catalog">
-                    <a className="flex justify-center md:justify-between">
-                      <Image src={ImageTrophee} width="200px" height="200px" />
+                    <a className="flex justify-center md:justify-between ">
+                      <Image
+                        src={ImageTrophee}
+                        width="200px"
+                        height="200px"
+                        className="rounded-xl"
+                      />
                     </a>
                   </Link>
                 </div>
@@ -79,3 +98,4 @@ export default function Home() {
     </div>
   );
 }
+// lg:  grid-cols-4 grid-rows-1 gap-6
