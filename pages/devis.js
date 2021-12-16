1ce3350a92f16800b4af023e85f1136fdc1f801e
solene Verrier <sosso711@hotmail.com>
2021-12-16 15:49:16 +0100

    design devis

--- pages/devis.js
+++ pages/devis.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Layout from "../components/Layout";
+import SendIcon from "@mui/icons-material/Send";
+import SaveIcon from "@mui/icons-material/Save";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 function devis() {
   return (
@@ -11,12 +14,12 @@ function devis() {
           </h1>
         </div>
         <form>
-          <label>devis</label>
           <div className="flex align items-center flex-col">
             <textarea
-              className="appearance-none block w-4/5  bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="address1"
-              name="address1"
+              placeholder="Votre message"
+              className="appearance-none block w-4/5  bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-10 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="demandeDevis"
+              name="demandeDevis"
               type="text"
             />
             <label> Pour quand ? :</label>
@@ -32,13 +35,28 @@ function devis() {
             Ajouter pièces jointes 3 maximum
           </button>
         </div>
+        <div>
+          <ul>
+            <li className="text-center">
+              test.pdf <DeleteForeverIcon className="ml-3" />
+            </li>
+            <li className="text-center">
+              test.pdf <DeleteForeverIcon className="ml-3" />
+            </li>
+
+            <li className="text-center">
+              test.pdf <DeleteForeverIcon className="ml-3" />
+            </li>
+          </ul>
+        </div>
 
         <div className="flex flex-row justify-around">
           <button className="bg-third w-1/4 h-10 flex justify-center rounded-3xl m-20 p-2 text-ml ">
-            Soumettre un devis
+            Soumettre un devis <SendIcon className="ml-10 rotate-90" />
           </button>
-          <button className="bg-third w-1/4 h-10 flex justify-center rounded-3xl m-20 p-2 text-ml">
-            Enregistrer ma demande pour continuer plus tard
+          <button className="bg-third w-2/5 h-10 flex justify-center rounded-3xl m-20 p-2 text-ml">
+            Enregistrer ma demande pour continuer plus tard <br />
+            <SaveIcon className="ml-10" />
           </button>
         </div>
       </Layout>
