74b996ada42e5f8f652345b526508d3485067cfd
Selena <selena.poun@gmail.com>
2021-12-17 10:10:11 +0100

    add buttons

--- components/SignupForm.jsx
+++ components/SignupForm.jsx
@@ -189,7 +189,7 @@ const SignupForm = () => {
             </div>
             <div className="w-full px-3">
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 mt-3">
-                Denomination
+                Dénomination
               </label>
               <input
                 className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
--- pages/devis_gestion.js
+++ pages/devis_gestion.js
@@ -3,23 +3,51 @@ import { IoIosAddCircle } from "react-icons/io";
 import { FaCloudDownloadAlt } from "react-icons/fa";
 import { RiDeleteBin5Fill } from "react-icons/ri";
 import next from "next";
+import Swal from "sweetalert2";
 
 export default function QuoteManagement() {
+  const handleClick = (e) => {
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Votre devis a bien été créée !",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
+  const Downloaad = (e) => {
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Votre devis a bien été téléchargé !",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
+
   return (
     <Layout>
-      <section className="bg-slate-50">
-        <h1 className="border-double border-4 border-thrid text-black rounded">
+      <section className="bg-slate-50 text-md">
+        <h1 className="border-double border-4 border-thrid text-black rounded place-items-center">
           Liste des devis validés ou en attente de validation
         </h1>
-        <h2 className="border-double border-4 border-thrid text-black rounded">
+        <h2 className="border-double border-4 border-thrid text-black rounded place-items-center">
           Liste des devis en cours de création
         </h2>
-        <IoIosAddCircle size={28} />
-        <button className="bg-thrid hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded-full cursor-pointer">
+
+        <button
+          className="bg-thrid hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded-full cursor-pointer"
+          onClick={handleClick}
+        >
+          <IoIosAddCircle size={28} />
           CRÉER UN DEVIS
         </button>
-        <FaCloudDownloadAlt size={28} />
-        <button className="bg-thrid hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded-full cursor-pointer">
+
+        <button
+          className="bg-thrid hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded-full cursor-pointer"
+          onClick={Downloaad}
+        >
+          <FaCloudDownloadAlt size={28} />
           TÉLÉCHARGER
         </button>
       </section>
