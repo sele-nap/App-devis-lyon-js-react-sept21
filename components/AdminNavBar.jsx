827d0169a7b889b9a809649e5e9b2cc856b4ac33
bahstiien <bastien.lecalvez@gmail.com>
2022-01-12 16:28:57 +0100

    add navbar

--- components/AdminNavBar.jsx
+++ components/AdminNavBar.jsx
@@ -1,44 +1,35 @@
 import React from "react";
 import Link from "next/link";
+import BackupTableIcon from "@mui/icons-material/BackupTable";
+import PermContactCalendarIcon from "@mui/icons-material/PermContactCalendar";
 
 const AdminNavBar = () => {
   return (
-    // <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer/>
-    // <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer />
-    <div className="min-h-screen">
-      <div className="antialiased bg-gray-100">
-        <div className="w-full text-gray-700 bg-white">
-          <div
-            x-data="{ open: true }"
-            className="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8"
-          >
-            <nav className="flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-end md:flex-row">
-              <a
-                className="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg "
-                href="#"
-              >
-                Blog
-              </a>
-              <a
-                className="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg "
-                href="#"
-              >
-                Portfolio
-              </a>
-              <a
-                className="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg "
-                href="#"
-              >
-                About
-              </a>
-              <a
-                className="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                href="#"
-              >
-                Contact
-              </a>
-              <div className="relative" x-data="{ open: true }"></div>
-            </nav>
+    <div className="flex justify-center mt-4">
+      <div className="border-b-2 mb-4">
+        <div className="antialiased bg-gray-100 dark-mode:bg-gray-900">
+          <div className="w-full mb-2 text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800">
+            <div
+              x-data="{ open: true }"
+              className="flex flex-col px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8"
+            >
+              <nav className="flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-end md:flex-row">
+                <a
+                  className="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-yellow-400 focus:bg-yellow-600 focus:outline-none focus:shadow-outline"
+                  href="/estimates"
+                >
+                  <BackupTableIcon />
+                  <span className="mx-2"> Mes devis</span>
+                </a>
+                <a
+                  className="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-yellow-400 focus:bg-yellow-600 focus:outline-none focus:shadow-outline"
+                  href="/users"
+                >
+                  <PermContactCalendarIcon />
+                  <span className="mx-2"> Mes contacts</span>
+                </a>
+              </nav>
+            </div>
           </div>
         </div>
       </div>
