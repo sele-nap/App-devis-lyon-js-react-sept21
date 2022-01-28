79ab319e6d8efb83b8148164523dcbbd9090565f
bahstiien <bastien.lecalvez@gmail.com>
2022-01-28 11:07:20 +0100

    Resolve status

--- components/ToggleButton.jsx
+++ components/ToggleButton.jsx
@@ -11,14 +11,14 @@ function ToggleButton({ e, handleChange }) {
       <label className="flex items-center justify-center">
         <input
           type="checkbox"
-          disabled={e.status === "WAITING_FOR_VALIDATION" ? true : false}
+          disabled={e.status === "VALIDATED" ? true : false}
           className="form-tick appearance-none bg-white bg-check h-6 w-6 border border-gray-300 rounded-md checked:bg-green-500 checked:border-transparent focus:outline-none"
-          defaultChecked={e.status === "WAITING_FOR_VALIDATION"}
+          defaultChecked={e.status === "VALIDATED"}
           onChange={async (event) => {
             const data = new FormData();
             data.append(
               "status",
-              e.status !== "VALIDATED" ? "WAITING_FOR_VALIDATION" : "TO_DO"
+              e.status !== "VALIDATED" ? "VALIDATED" : "WAITING_FOR_VALIDATION"
             );
 
             axios.patch(`/api/estimate/${e.id}`, data).then(handleChange);
