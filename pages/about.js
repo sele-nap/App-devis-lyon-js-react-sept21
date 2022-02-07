67850f053674bc1b93ad38e2256eb9c793f41a98
Selena <selena.poun@gmail.com>
2022-02-07 10:07:15 +0100

    update pages

--- pages/about.js
+++ pages/about.js
@@ -4,41 +4,41 @@ import next from "next";
 export default function About() {
   return (
     <Layout title="Qui sommes nous ?">
-      <div className="flex justify-center"></div>
-
-      <p className="text-justify mx-20">
-        {" "}
-        Cat ipsum dolor sit amet, scratch. Why must they do that fat baby cat
-        best buddy little guy yet prow?? ew dog you drink from the toilet, yum
-        yum warm milk hotter pls, ouch too hot, so hide at bottom of staircase
-        to trip human so eat the rubberband fall asleep on the washing machine.
-        Pee in humans bed until he cleans the litter box allways wanting food,
-        chase dog then run away. If it smells like fish eat as much as you wish.
-        Why use post when this sofa is here loved it, hated it, loved it, hated
-        it and hit you unexpectedly. Meowing chowing and wowing crash against
-        wall but walk away like nothing happened catto munch salmono but one of
-        these days im going to get that red dot, just you wait and see and iâ€™m
-        so hungry iâ€™m so hungry but ew not for that for i will ruin the couch
-        with my claws. Love blinks and purr purr purr purr yawn scratch and man
-        running from cops stops to pet cats, goes to jail yet intrigued by the
-        shower stare at ceiling light. Cough hairball on conveniently placed
-        pants experiences short bursts of poo-phoria after going to the loo swat
-        at dog rub face on owner for paw at beetle and eat it before it gets
-        away so mewl for food at 4am get suspicious of own shadow then go play
-        with toilette paper. Peer out window, chatter at birds, lure them to
-        mouth whenever a door is opened, rush in before the human. Stinky cat
-        cat gets stuck in tree firefighters try to get cat down firefighters get
-        stuck in tree cat eats firefighters slippers for catto munch salmono,
-        yet i is not fat, i is fluffy yet catch eat throw up catch eat throw up
-        bad birds, so what a cat-ass-trophy!, destroy couch as revenge. Steal
-        the warm chair right after you get up. Milk the cow
-        mmmmmmmmmeeeeeeeeooooooooowwwwwwww and stand in doorway, unwilling to
-        chose whether to stay in or go out sleep in the bathroom sink eat prawns
-        daintily with a claw then lick paws clean wash down prawns with a lap of
-        carnation milk then retire to the warmest spot on the couch to claw at
-        the fabric before taking a catnap so run off table persian cat jump eat
-        fish.
-      </p>
+      <div className="flex justify-center">
+        <p className="text-justify mx-20">
+          Cat ipsum dolor sit amet, scratch. Why must they do that fat baby cat
+          best buddy little guy yet prow?? ew dog you drink from the toilet, yum
+          yum warm milk hotter pls, ouch too hot, so hide at bottom of staircase
+          to trip human so eat the rubberband fall asleep on the washing
+          machine. Pee in humans bed until he cleans the litter box allways
+          wanting food, chase dog then run away. If it smells like fish eat as
+          much as you wish. Why use post when this sofa is here loved it, hated
+          it, loved it, hated it and hit you unexpectedly. Meowing chowing and
+          wowing crash against wall but walk away like nothing happened catto
+          munch salmono but one of these days im going to get that red dot, just
+          you wait and see and iâ€™m so hungry iâ€™m so hungry but ew not for
+          that for i will ruin the couch with my claws. Love blinks and purr
+          purr purr purr yawn scratch and man running from cops stops to pet
+          cats, goes to jail yet intrigued by the shower stare at ceiling light.
+          Cough hairball on conveniently placed pants experiences short bursts
+          of poo-phoria after going to the loo swat at dog rub face on owner for
+          paw at beetle and eat it before it gets away so mewl for food at 4am
+          get suspicious of own shadow then go play with toilette paper. Peer
+          out window, chatter at birds, lure them to mouth whenever a door is
+          opened, rush in before the human. Stinky cat cat gets stuck in tree
+          firefighters try to get cat down firefighters get stuck in tree cat
+          eats firefighters slippers for catto munch salmono, yet i is not fat,
+          i is fluffy yet catch eat throw up catch eat throw up bad birds, so
+          what a cat-ass-trophy!, destroy couch as revenge. Steal the warm chair
+          right after you get up. Milk the cow
+          mmmmmmmmmeeeeeeeeooooooooowwwwwwww and stand in doorway, unwilling to
+          chose whether to stay in or go out sleep in the bathroom sink eat
+          prawns daintily with a claw then lick paws clean wash down prawns with
+          a lap of carnation milk then retire to the warmest spot on the couch
+          to claw at the fabric before taking a catnap so run off table persian
+          cat jump eat fish.
+        </p>
+      </div>
     </Layout>
   );
 }
