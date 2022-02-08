bf0f7638a9daece9b55417aa305f7c4760dc1dcf
Selena <selena.poun@gmail.com>
2022-02-08 10:01:56 +0100

    update margin footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="bg-third">
-      <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto py-6 px-4 overflow-hidden sm:px-6 lg:px-8">
         <nav
           className="-mx-5 -my-2 flex flex-wrap justify-center"
           aria-label="Footer"
@@ -26,7 +26,7 @@ export default function Footer() {
           <div className="px-5 py-2">
             <Link href="./about">
               <a className="px-4 py-2 mt-2 text-sm font-semibold text-gray-900  rounded-lg  md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-yellow-400 focus:bg-yellow-400 focus:outline-none focus:shadow-outline">
-                Qui sommes nous
+                Qui sommes-nous
               </a>
             </Link>
           </div>
